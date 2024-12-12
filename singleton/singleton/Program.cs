1f1d5698921c8f73143f5561cc54ba5f16c2103b
riyaj-optimus <riya.j@optimusinfo.com>
2024-12-12 18:40:19 +0530

    singleton

---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        
+        Parallel.Invoke(
+            () => details(),
+            () => details(),
+            () => details()
+            );
+
+    }
+    public static void details()
+    {
+        Singleton obj = Singleton.Instance();
+        obj.meth();
+    }
+}
+public sealed  class Singleton
+{
+    private static int Counter = 0;
+    private static Singleton inst = null;
+
+    private Singleton()
+    {
+        Counter++;
+        Console.WriteLine("Counter is : "+Counter);
+    }
+
+    public static Singleton Instance() { 
+        if(inst == null)
+        {
+            inst = new Singleton();
+
+        }
+        return inst;
+    }
+    //another method 
+    public void meth()
+    {
+        Console.WriteLine("Singleton's Method");
+    }
+    
+}
+
+
