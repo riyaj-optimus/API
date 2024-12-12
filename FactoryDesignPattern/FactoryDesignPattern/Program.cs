3f237c783a6b17aee0a553043bf9d84ab84c1a0f
riyaj-optimus <riya.j@optimusinfo.com>
2024-12-12 18:39:18 +0530

    init

---
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using FactoryDesignPattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string card = "MoneyBack";
+        CreditCard cardDetails = null;
+
+        if (card == "MoneyBack")
+        {
+            cardDetails = new MoneyBack();
+        }
+        else if (card == "Titanium")
+        {
+            cardDetails = new Titanium();
+        }
+        else if (card == "Platinum")
+        {
+            cardDetails = new Platinum();
+        }
+
+        if(cardDetails != null)
+        {
+            Console.WriteLine("CardType : " + cardDetails.GetCardType());
+            Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+            Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+        }
+        else
+        {
+            Console.Write("Invalid Card Type");
+        }
+
+
+
+    }
+}
